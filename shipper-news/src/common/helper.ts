8302a6d533937d40e3d0c3260958a90a75c1f19c
akshay solanki <akshay.solanki@shipsy.co.in>
2019-12-13 20:08:54 +0530

    fix

--- shipper-news/src/common/helper.ts
+++ shipper-news/src/common/helper.ts
@@ -1,6 +1,37 @@
 import * as camelCase from "camelCase";
 
 export class Helper{
+    static stringConstructor = ''.constructor;
+	static arrayConstructor = [].constructor;
+	static objectConstructor = {}.constructor;
+	static numberConstructor = (0).constructor;
+	static dateConstructor = new Date().constructor;
+    static isVoid(obj) {
+		switch (typeof obj) {
+			case 'undefined':
+				return true;
+			case 'object':
+				for (const x in obj) {
+					if (obj.hasOwnProperty(x)) {
+						return false;
+					}
+					return true;
+				}
+				return true;
+			case 'number':
+			case 'boolean':
+				return false;
+			case 'string':
+				if (obj === '' || obj === 'null' || obj === 'undefined') {
+					return true;
+				}
+				return false;
+			/* falls through */
+			default:
+				return false;
+		}
+    }
+    
     static convertToCamelCaseObject(data: any) {
 		let newData: any;
 		let origKey: string;
@@ -26,5 +57,29 @@ export class Helper{
 			}
 		}
 		return newData;
+    }
+    static whatIsIt(object) {
+		if (object === null) {
+			return 'null';
+		}
+		if (object === undefined) {
+			return 'undefined';
+		}
+		if (object.constructor === this.stringConstructor) {
+			return 'String';
+		}
+		if (object.constructor === this.arrayConstructor) {
+			return 'Array';
+		}
+		if (object.constructor === this.objectConstructor || typeof object === 'object') {
+			return 'Object';
+		}
+		if (object.constructor === this.numberConstructor) {
+			return 'Number';
+		}
+		if (object.constructor === this.dateConstructor) {
+			return 'Date';
+		}
+		return 'Other';
 	}
 }
